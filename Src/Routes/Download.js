6a69ab2ebcf70fc6987b0aa5d5f13570aed5bff1
ZiaXdev <63851399+ZiaXDev@users.noreply.github.com>
2020-06-12 13:59:31 +0530

    Add files via upload

---
+++ Src/Routes/Download.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class Download extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="row mt-4">
+        <div className="col-md-12 text-center">
+          <h4 className="Righteous">Under construction</h4>
+        </div>
+      </div>
+    );
+  }
+}
